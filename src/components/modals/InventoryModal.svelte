307fd7561fbc94104ce74a0a03a3dec7d0e771ac
Grober Gonzales <grober.gonzales@khipu.pe>
2025-10-01 12:52:17 -0500

    feat: implement item drop functionality in InventoryModal; track mouse position and handle key events for item interactions

--- src/components/modals/InventoryModal.svelte
+++ src/components/modals/InventoryModal.svelte
@@ -10,8 +10,11 @@
 	import { RecordEcs } from '#/ecs/lib/Record.ecs';
 	import type { PlayerState } from '#/state/player.state';
 	import { SvelteMap } from 'svelte/reactivity';
+	import type { IVec2 } from '#/utils/math.util';
 
 	let worldOp = getContext<Option<WorldEcs>>(WorldEcs.name);
+
+	let mousePos: IVec2 = { x: 0, y: 0 };
 
 	let itemState = new SvelteMap<
 		string,
@@ -63,8 +66,51 @@
 		};
 	});
 
-	let items = ['A', 'B', 'C', 'D'];
+	function onMouseMove(event: MouseEvent) {
+		mousePos = { x: event.clientX, y: event.clientY };
+	}
+
+	const itemDropHandler = (id: string) => (itemDiv: HTMLDivElement) => {
+		const world = worldOp.raw();
+		if (!world) {
+			return () => {};
+		}
+
+		const room = world
+			.get(ColyseusClientEcs)
+			.pick('connection')
+			.collapse()
+			.map((c) => c.room)
+			.raw();
+		if (!room) {
+			return () => {};
+		}
+
+		const onKeyQ = (event: KeyboardEvent) => {
+			// Check if the itemDiv is hovered
+			const rect = itemDiv.getBoundingClientRect();
+
+			if (
+				mousePos.x >= rect.left &&
+				mousePos.x <= rect.right &&
+				mousePos.y >= rect.top &&
+				mousePos.y <= rect.bottom &&
+				event.key === 'q'
+			) {
+				room.send('client:action', { type: 'drop', itemId: id });
+				itemState.delete(id);
+			}
+		};
+
+		document.addEventListener('keydown', onKeyQ);
+
+		return () => {
+			document.removeEventListener('keydown', onKeyQ);
+		};
+	};
 </script>
+
+<svelte:document on:mousemove={onMouseMove} />
 
 <div class="grid grid-cols-6 gap-2 bg-gray-800 p-4 [direction:reverse]">
 	{#each new Array(20) as _, i}
@@ -73,7 +119,9 @@
 			class="flex aspect-square size-16 items-center justify-center rounded-md border border-gray-600 bg-gray-700"
 		>
 			{#if item}
-				<div class="Item">{item.type}</div>
+				<div class="Item" {@attach itemDropHandler(i.toString())}>
+					{item.type}
+				</div>
 			{:else}
 				<div class="Empty">Empty</div>
 			{/if}
