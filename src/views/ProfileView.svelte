a112423d9fec1c81f5879d294ad8bc2b0f7f459d
Grober Gonzales <GonzalsDLCGrober@gmail.com>
2025-10-06 18:04:19 -0500

    feat: add logout functionality to ProfileView; implement logoutHandler and update button for user logout

--- src/views/ProfileView.svelte
+++ src/views/ProfileView.svelte
@@ -10,6 +10,15 @@
 	onMount(() => {
 		gameInputContext.disabled = true;
 	});
+
+	function logoutHandler() {
+		fetch(import.meta.env.VITE_API_URL + '/api/v1/auth/logout', {
+			method: 'POST',
+			credentials: 'include',
+		}).then(() => {
+			window.location.href = '/';
+		});
+	}
 </script>
 
 <section class="flex h-full w-full flex-col items-center gap-4 p-4">
@@ -20,13 +29,30 @@
 		<div class="flex-1">
 			<SkinSelector />
 		</div>
-		<div class="flex flex-1">
+		<div class="flex flex-1 gap-2">
 			<button
 				class="pointer-events-auto"
 				onclick={() => router.changeRoute('/game')}
 			>
 				Go to Game
 			</button>
+			<button
+				type='button'
+				onclick={logoutHandler}
+			> Logout </button>
 		</div>
 	</div>
 </section>
+
+<style>
+	button {
+		transition: background-color 0.3s ease;
+		cursor: pointer;
+		border: none;
+		border-radius: 0.375rem;
+		background-color: #3b82f6;
+		padding: 0.5rem 1rem;
+		color: white;
+		font-size: 1rem;
+	}
+</style>
